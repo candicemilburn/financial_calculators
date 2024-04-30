b773a8f0062a6b879154b04bac08b21f10dffc34
Candice Milburn <candicemilburn@hotmail.com>
2024-04-30 10:58:33 -0500

    set up calculator for for future value, added CSS to all sheets

--- futureValueCalc.js
+++ futureValueCalc.js
@@ -1,3 +1,14 @@
 "use strict"
-console.log("it is working!");
+console.log("code is running inside futureValueCalc.js");
 
+window.onload = function () {
+//look through the HTML doc for something with the id of futureValueForm 
+//and store it in a variable 
+const futureValueForm= document.querySelector("futureValueForm");
+
+futureValueForm.addEventListener("submit",calcFutureValue);
+}
+
+function calcFutureValue() {
+    console.log ("candy did that")
+}
