427631432a3a09a16990a53734f287ce7b9e6bfc
Candice Milburn <candicemilburn@hotmail.com>
2024-04-29 21:04:02 -0500

    added clear button and interest field to mortge calc

--- script/mortageCalc.js
+++ script/mortageCalc.js
@@ -9,19 +9,29 @@ function init() {
     let clearButton = document.querySelector("#clearButton");
 
     calcButton.addEventListener("click", calculate)
-    clearButton.addEventListener("click", clear)
+    clearButton.addEventListener("click", clearButton)
 }
 
 function calculate() {
     let aprField = Number(document.querySelector("#aprField").value)
     let loantermField = Number(document.querySelector("#loantermField").value)
     let loanamountField = Number(document.querySelector("#loanamountField").value);
-    let monthlyInterestRate = (aprField / 100 / 12)
-    let totalNumOfPayments = (loantermField * 12)
+    let monthlyInterestRate = (aprField / 100 / 12);
+    let totalNumOfPayments = (loantermField * 12);
     let result = loanamountField * monthlyInterestRate * ((1 + monthlyInterestRate) ** totalNumOfPayments) / (((1 + monthlyInterestRate) ** totalNumOfPayments) - 1);
+    let totalInterestPaid = result * totalNumOfPayments - loanamountField;
+    let clearButton
 
     result = result.toFixed(2);
 
+    totalInterestPaid =totalInterestPaid.toFixed(2)
+
     document.querySelector("#result").value = result;
 
+    document.querySelector("#totalInterestPaid").value = totalInterestPaid;
+  
 }
+function clearButton() {
+    document.getElementById("reset").reset();
+}
+
